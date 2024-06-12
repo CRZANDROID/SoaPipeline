493e13d632a940e6c1ba297fb92604425c73f638
Josue Galdamez <101213746+CRZANDROID@users.noreply.github.com>
2024-06-11 18:11:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-  res.send('Hello, World! x10');
+  res.send('Hello, World! x11');
 });
 
 app.listen(port, () => {
